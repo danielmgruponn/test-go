c2783e04851207318569ff85b1c28b5c74ae3731
yerson <yargote.gruponn@gmail.com>
2024-09-02 15:50:37 -0500

    fix: Update WebRTCHandler to use string for userId and improve logging for debugging purposes

--- internal/handlers/webrtc_handler.go
+++ internal/handlers/webrtc_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"strconv"
 	"sync"
@@ -39,8 +38,8 @@ func (h *WebRTCHandler) HandlerWebRTC() fiber.Handler {
 		h.clients.Store(userIdInt, ws)
 
 		defer func() {
-			log.Printf("User %d disconnected from WebRTC\n", userIdInt)
-			h.clients.Delete(userIdInt)
+			log.Printf("User %s disconnected from WebRTC\n", userId)
+			h.clients.Delete(userId)
 			ws.Close()
 		}()
 
@@ -56,41 +55,56 @@ func (h *WebRTCHandler) HandlerWebRTC() fiber.Handler {
 				log.Printf("JSON Unmarshal error: %v\n", err)
 				continue
 			}
-			h.handleSignal(userIdInt, message)
+			h.handleSignal(userId, message)
 		}
 	})
 }
 
-func (h *WebRTCHandler) handleSignal(userId uint, message map[string]interface{}) {
-	log.Printf("Handling signal from user %d\n", userId)
+func (h *WebRTCHandler) handleSignal(userId string, message map[string]interface{}) {
+	log.Printf("Handling signal from user %s\n", userId)
 	log.Printf("Message: %v\n", message)
 	messageType, ok := message["type"].(string)
 	if !ok {
-		fmt.Println("Invalid message type")
+		log.Println("Invalid message type")
 		return
 	}
 
 	to, ok := message["to"].(string)
 	if !ok {
-		fmt.Println("Invalid 'to' field")
+		log.Println("Invalid 'to' field")
 		return
 	}
 
-	targetConn, ok := h.clients.Load(to)
+	// show all clients
+	log.Printf("userId: %s", userId)
+	log.Printf("to: %s", to)
+	log.Println("All clients:")
+	h.clients.Range(func(key, value any) bool {
+		log.Printf("Client %s", key)
+		return true
+	})
+	// convert to uint
+	toUint, err := strconv.Atoi(to)
+	if err != nil {
+		log.Println("Error converting to uint:", err)
+		return
+	}
+	toUintInt := uint(toUint)
+	targetConn, ok := h.clients.Load(toUintInt)
 	if !ok {
-		fmt.Printf("Target client %s not found\n", to)
+		log.Printf("Target client %s not found\n", to)
 		return
 	}
 
 	ws, ok := targetConn.(*websocket.Conn)
 	if !ok {
-		fmt.Printf("Invalid connection type for client %s\n", to)
+		log.Printf("Invalid connection type for client %s\n", to)
 		return
 	}
 
 	signalData, err := json.Marshal(message["signal"])
 	if err != nil {
-		fmt.Println("Error marshalling signal data: ", err)
+		log.Println("Error marshalling signal data: ", err)
 		return
 	}
 
@@ -102,6 +116,6 @@ func (h *WebRTCHandler) handleSignal(userId uint, message map[string]interface{}
 
 	err = ws.WriteJSON(response)
 	if err != nil {
-		fmt.Printf("Error sending %s message to target: %v\n", messageType, err)
+		log.Printf("Error sending %s message to target: %v\n", messageType, err)
 	}
 }
--- internal/routes/api_user.go
+++ internal/routes/api_user.go
@@ -11,7 +11,7 @@ import (
 func SetupRoutes(app *fiber.App, userController *handlers.UserHandler, mnsController *handlers.MessageHandler, fileController *handlers.FileHandler) {
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:8080/, http://localhost:3000",
+		AllowOrigins:     "https://7faa-190-84-88-236.ngrok-free.app",
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
