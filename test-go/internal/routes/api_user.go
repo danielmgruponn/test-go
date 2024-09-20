1a342a9e4d7c9cd1cfc01077650023ce5cb509a5
danielmgruponn <143098579+danielmgruponn@users.noreply.github.com>
2024-09-20 14:26:25 -0500

    SGI: Docker my Ip

--- test-go/internal/routes/api_user.go
+++ test-go/internal/routes/api_user.go
@@ -11,7 +11,7 @@ import (
 func SetupRoutes(app *fiber.App, userController *handlers.UserHandler, mnsController *handlers.MessageHandler, fileController *handlers.FileHandler) {
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000/",
+		AllowOrigins:     "http://192.168.1.4:3000/",
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
